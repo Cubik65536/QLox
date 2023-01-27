54742326c1826bfec4f1bce398ec0afe7f03fcb8
Cubik <cubik65536@cubik65536.top>
2023-01-26 23:38:31 -0500

    feat: Add the `AstPrinter` for inspect tree structure

---
+++ src/main/kotlin/org/qianq/qlox/AstPrinter.kt
@@ -0,0 +1,37 @@
+package org.qianq.qlox
+
+class AstPrinter: Expr.Visitor<String> {
+    private fun parenthesize(name: String, vararg exprs: Expr): String {
+        val stringBuilder = StringBuilder()
+
+        stringBuilder.append("(").append(name)
+        for (expr in exprs) {
+            stringBuilder.append(" ")
+            stringBuilder.append(expr.accept(this))
+        }
+        stringBuilder.append(")")
+
+        return stringBuilder.toString()
+    }
+
+    fun print(expr: Expr): String {
+        return expr.accept(this)
+    }
+
+    override fun visitExpr(expr: Binary): String {
+        return parenthesize(expr.operator.lexeme, expr.left, expr.right)
+    }
+
+    override fun visitExpr(expr: Grouping): String {
+        return parenthesize("group", expr.expression)
+    }
+
+    override fun visitExpr(expr: Literal): String {
+        if (expr.value == null) return "nil"
+        return expr.value.toString()
+    }
+
+    override fun visitExpr(expr: Unary): String {
+        return parenthesize(expr.operator.lexeme, expr.right)
+    }
+}

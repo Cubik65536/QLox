a8e609cd3eea4a67eefa37a874904df5e294dedf
Cubik <cubik65536@cubik65536.top>
2023-02-03 15:34:25 -0500

    feat(Interpreter): Add `Interpreter` class

---
+++ src/main/kotlin/org/qianq/qlox/Interpreter.kt
@@ -0,0 +1,19 @@
+package org.qianq.qlox
+
+class Interpreter: Expr.Visitor<Any> {
+    override fun visitExpr(expr: Binary): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitExpr(expr: Grouping): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitExpr(expr: Literal): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun visitExpr(expr: Unary): Any {
+        TODO("Not yet implemented")
+    }
+}

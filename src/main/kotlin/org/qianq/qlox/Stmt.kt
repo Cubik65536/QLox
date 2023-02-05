cf137db5a0a2c80cdaf142b50f3900946e009498
Cubik <cubik65536@cubik65536.top>
2023-02-04 22:59:25 -0500

    feat(Stmt): Add statement syntax trees

---
+++ src/main/kotlin/org/qianq/qlox/Stmt.kt
@@ -0,0 +1,24 @@
+package org.qianq.qlox
+
+import org.qianq.qlox.token.Token
+
+abstract class Stmt {
+    abstract fun <R> accept(visitor: Visitor<R>): R
+    interface Visitor<R> {
+        fun visitStmt(stmt: Expression): R
+        fun visitStmt(stmt: Print): R
+    }
+}
+
+class Expression(val expression: Expr) : Stmt() {
+    override fun <R> accept(visitor: Visitor<R>): R {
+        return visitor.visitStmt(this)
+    }
+}
+
+class Print(val expression: Expr) : Stmt() {
+    override fun <R> accept(visitor: Visitor<R>): R {
+        return visitor.visitStmt(this)
+    }
+}
+

46cfb7317caa7fe859e55bdc789e16eb77490906
Cubik <cubik65536@cubik65536.top>
2023-02-05 01:20:40 -0500

    feat(Environment): Add `enclosing` environment for scope variables

--- src/main/kotlin/org/qianq/qlox/Environment.kt
+++ src/main/kotlin/org/qianq/qlox/Environment.kt
@@ -3,7 +3,17 @@ package org.qianq.qlox
 import org.qianq.qlox.token.Token
 
 class Environment {
+    val enclosing: Environment?
+
     private val values: MutableMap<String, Any?> = mutableMapOf()
+
+    constructor() {
+        enclosing = null
+    }
+
+    constructor(enclosing: Environment) {
+        this.enclosing = enclosing
+    }
 
     fun define(name: String, value: Any?) {
         values[name] = value
@@ -14,12 +24,19 @@ class Environment {
             return values[name.lexeme]
         }
 
+        if (enclosing != null) return enclosing.get(name)
+
         throw RuntimeError(name, "Undefined variable '${name.lexeme}'.")
     }
 
     fun assign(name: Token, value: Any?) {
         if (values.containsKey(name.lexeme)) {
             values[name.lexeme] = value
+            return
+        }
+
+        if (enclosing != null) {
+            enclosing.assign(name, value)
             return
         }
 
